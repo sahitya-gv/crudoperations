32781d57750cacb1bc024a28837a17a946b394a8
GangaSahitya G.v <gvsahitya195@gmail.com>
2026-08-25 12:46:36 +0530

    Disable DEBUG mode in settings.py

--- crudoperation/settings.py
+++ crudoperation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sg0urn)1s(yrvg2t@ho(-$=@#3-scdw&v3b#-0ys-)kd%+(hod'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
